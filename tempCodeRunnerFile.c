e8a15ee333ed33b528c4959b92a7bc0bc7053671
Juliana Sophia <jsqpabonita@gmail.com>
2025-11-05 09:38:34 +0000

    sample math calcns

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
 
-    int age = 0;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    printf("Your age is %d.", age);
+    float radius = 0.0f;
+
+    printf("Enter the radius: ");
+    scanf("%f", &radius);
+
+    float pi = 3.14159265358979323;
+    float radius_squared = pow(radius, 2);
+    float area = pi * radius_squared;
+    float surface_area = 4 * area;
+    float volume = (4 / 3) * pi * pow(radius, 3);
+
+    printf("Area: %.2f\n", area);
+    printf("Surface Area: %.2f\n", surface_area);
+    printf("Volume: %.2f\n", volume);
+
     return 0;
 }
