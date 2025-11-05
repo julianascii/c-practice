2d6bcd37d649861ec45c6d228e2b94352e2f01c7
Juliana Sophia <jsqpabonita@gmail.com>
2025-11-05 04:50:10 +0000

    start printing and commenting

---
+++ print_and_comment.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// this is a comment
+
+/*
+this is also a comment but multi-line
+*/
+
+int main() {
+
+    printf("Hello, world! \n I am learning C. Yay!");
+    printf("juwiana");
+    return 0;
+}
